bf220b8fcd446fe2a3418d2b7771e95a64f4e44b
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-09-20 18:41:49 -0500

    Add starter database tables

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "photos" (
+  "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY
+);
+
+CREATE TABLE "users" (
+  "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+  "username" varchar(20) NOT NULL UNIQUE,
+  "password" varchar(20) NOT NULL
+);
+
+CREATE TABLE "captions" (
+  "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+  "photo_id" uuid REFERENCES photos(id) NOT NULL,
+  "user_id" uuid REFERENCES users(id) NOT NULL,
+  "caption" varchar(500) NOT NULL
+)
